1cb7f8916d215671c76cdbd533fa312f4c6a2a57
James <Hal@Jamess-MacBook-Pro.local>
2016-08-17 09:41:52 -0500

    FirstCommit

---
+++ DiveApp/Diver.swift
@@ -0,0 +1,22 @@
+//
+//  Diver.swift
+//  DiveApp
+//
+//  Created by James O'Connor on 8/15/16.
+//  Copyright © 2016 James O'Connor. All rights reserved.
+//
+
+//import Foundation
+//
+//class Diver: NSObject {
+//    
+//    let userName :String?
+//    let firstName :String?
+//    let lastName :String?
+//
+//    
+//    
+//        
+//    }
+//
+//}
